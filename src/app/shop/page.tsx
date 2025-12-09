3a317a6385073e5da9fcb7c5da68016625ce9644
Nickopusan <nickowork13@gmail.com>
2025-12-09 12:19:06 +0700

    fix shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,15 +1,16 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import MainShop from "@/components/shop/MainShop";
+import { Suspense } from "react";
 
 export default function ShopPage() {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Navbar />
       <main className="bg-white/50 dark:bg-zinc-900 py-40">
         <MainShop />
       </main>
       <Footer />
-    </>
+    </Suspense>
   );
 }
