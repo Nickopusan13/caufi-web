f14716af0f09376eebfa880ef63d9aa75b1750bb
Nickopusan <nickowork13@gmail.com>
2025-12-14 16:13:22 +0700

    fix docker

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,8 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/navbar/Navbar";
 import type { Metadata } from "next";
 
+export const metadata: Metadata = {};
+
 export default function AboutPage() {
   return (
     <>
